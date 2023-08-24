0ba459ba398b5bccf9daafac33e8caf6392defe6
Giuntoli <luca.giuntoli@accenture.com>
2023-08-24 10:20:35 +0200

    3 notfound redirect complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CardComponentComponent } from './card-component/card-component.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-const routes: Routes = [{path: 'postslist', component: CardComponentComponent}];
+const routes: Routes = [
+  { path: 'postslist', component: CardComponentComponent },
+  { path: '404', component: NotfoundComponent },
+  { path: '**', redirectTo: '/404' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
