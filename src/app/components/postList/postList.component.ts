3e9b0277483e224de4247e5fe89bbf008bb5d2b8
Giuntoli <luca.giuntoli@accenture.com>
2023-09-11 16:01:34 +0200

    type IPost updated

--- src/app/components/postList/postList.component.ts
+++ src/app/components/postList/postList.component.ts
@@ -10,8 +10,7 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./postList.component.css'],
 })
 export class PostListComponent implements OnInit {
-  //posts: IPost[] = [];
-  posts: any = [];
+  posts: IPost[] = [];
   postId: any;
   isConfirmationModalOpen = false;
   selectedPostId: string | null = null;
@@ -26,6 +25,62 @@ export class PostListComponent implements OnInit {
       });
       console.log(this.posts);
     });
+
+    // const firstGroup$ = timer(1000).pipe(
+    //   switchMap(() =>
+    //     this.firebase.getPost().pipe(
+    //       map((data) => {
+    //         const filteredData = data.filter((dataelem) => dataelem.key <= 10);
+
+    //         return filteredData;
+    //       })
+    //     )
+    //   )
+    // );
+
+    // const secondGroup$ = timer(2000).pipe(
+    //   switchMap(() =>
+    //     this.firebase
+    //       .getPost()
+    //       .pipe(
+    //         map((data) =>
+    //           data.filter((dataelem) => dataelem.id > 10 && dataelem.id <= 20)
+    //         )
+    //       )
+    //   )
+    // );
+
+    // const thirdGroup$ = timer(3000).pipe(
+    //   switchMap(() =>
+    //     this.firebase
+    //       .getPost()
+    //       .pipe(
+    //         map((data) =>
+    //           data.filter((dataelem) => dataelem.id > 20 && dataelem.id <= 30)
+    //         )
+    //       )
+    //   )
+    // );
+
+    // firstGroup$
+    //   .pipe(
+    //     tap((firstGroup) => (this.posts = firstGroup)),
+    //     switchMap(() =>
+    //       secondGroup$.pipe(
+    //         tap(
+    //           (secondGroup) => (this.posts = [...this.posts, ...secondGroup])
+    //         ),
+    //         switchMap(() =>
+    //           thirdGroup$.pipe(
+    //             tap(
+    //               (thirdGroup) => (this.posts = [...this.posts, ...thirdGroup])
+    //             )
+    //           )
+    //         )
+    //       )
+    //     )
+    //   )
+    //   .subscribe(noop);
   }
 
   openConfirm(postId: string): void {
@@ -63,60 +118,3 @@ export class PostListComponent implements OnInit {
     });
   }
 }
-
-
-    // const firstGroup$ = timer(1000).pipe(
-    //   switchMap(() =>
-    //     this.dataService.getData().pipe(
-    //       map((data) => {
-    //         const filteredData = data.filter((dataelem) => dataelem.id <= 10);
-
-    //         return filteredData;
-    //       })
-    //     )
-    //   )
-    // );
-
-    // const secondGroup$ = timer(2000).pipe(
-    //   switchMap(() =>
-    //     this.dataService
-    //       .getData()
-    //       .pipe(
-    //         map((data) =>
-    //           data.filter((dataelem) => dataelem.id > 10 && dataelem.id <= 20)
-    //         )
-    //       )
-    //   )
-    // );
-
-    // const thirdGroup$ = timer(3000).pipe(
-    //   switchMap(() =>
-    //     this.dataService
-    //       .getData()
-    //       .pipe(
-    //         map((data) =>
-    //           data.filter((dataelem) => dataelem.id > 20 && dataelem.id <= 30)
-    //         )
-    //       )
-    //   )
-    // );
-
-    // firstGroup$
-    //   .pipe(
-    //     tap((firstGroup) => (this.posts = firstGroup)),
-    //     switchMap(() =>
-    //       secondGroup$.pipe(
-    //         tap(
-    //           (secondGroup) => (this.posts = [...this.posts, ...secondGroup])
-    //         ),
-    //         switchMap(() =>
-    //           thirdGroup$.pipe(
-    //             tap(
-    //               (thirdGroup) => (this.posts = [...this.posts, ...thirdGroup])
-    //             )
-    //           )
-    //         )
-    //       )
-    //     )
-    //   )
-    //   .subscribe(noop);
